5ac3cdb8e649569d74e35ec421f6a794990ce233
beckfordblack <vk6sgysw7p@privaterelay.appleid.com>
2026-09-26 17:50:42 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-document.addEventListener('dblclick', function(e) {
-    e.preventDefault();
-}, { passive: false });
+const GAME_WIDTH = 1080;
+const GAME_HEIGHT = 1920;
+
+const GRID_COLS = 5;
+const GRID_ROWS = 8;
+const GRID_SIZE = 160;
+const GRID_GAP = 4;
 
 const gameLayer = document.getElementById("game");
 const titleLayer = document.getElementById("title");
@@ -10,39 +14,9 @@ const startScreen = document.getElementById("startScreen");
 const gameOverScreen = document.getElementById("gameOverScreen");
 const startButton = document.getElementById("startButton");
 const retryButton = document.getElementById("retryButton");
-const finalScore = document.getElementById("finalScore");
 const homeButton = document.getElementById("homeButton");
 const skipButton = document.getElementById("skipButton");
-
-startButton.addEventListener("click", startGame);
-retryButton.addEventListener("click", startGame);
-homeButton.addEventListener("click", home)
-skipButton.addEventListener("click", skip)
-
-const GAME_WIDTH = 1080;
-const GAME_HEIGHT = 1920;
-
-gameLayer.style.width = `${GAME_WIDTH}px`;
-gameLayer.style.height = `${GAME_HEIGHT}px`;
-
-function resizeGame() {
-    const scaleX = window.innerWidth / GAME_WIDTH;
-    const scaleY = window.innerHeight / GAME_HEIGHT;
-    const scale = Math.min(scaleX, scaleY);
-    gameLayer.style.transform = `scale(${scale})`;
-    return scale;
-}
-
-window.addEventListener("resize", resizeGame);
-const GAME_SCALE = resizeGame();
-
-const GRID_COLS = 5;
-const GRID_ROWS = 8;
-const GRID_SIZE = 160;
-const GRID_GAP = 4;
-
-boardLayer.style.width = `${GRID_COLS * (GRID_SIZE + GRID_GAP) - GRID_GAP}px`;
-boardLayer.style.height = `${GRID_ROWS * (GRID_SIZE + GRID_GAP) - GRID_GAP}px`;
+const finalScore = document.getElementById("finalScore");
 
 const ELEMENTS = [
     {text: "H", color: "#448787"},
@@ -141,95 +115,150 @@ const MOLECULES = [
 let isPointer = false;
 let isPlaying = false;
 let isGameOver = false;
+
 let score = 0;
-const selectPieces = [];
 let titleTimer;
+
+const selectedPieces = [];
+
 let grid = Array.from(
     {length: GRID_ROWS},
     () => Array(GRID_COLS).fill(null)
 );
 
+gameLayer.style.width = `${GAME_WIDTH}px`;
+gameLayer.style.height = `${GAME_HEIGHT}px`;
+
+boardLayer.style.width = `${GRID_COLS * (GRID_SIZE + GRID_GAP) - GRID_GAP}px`;
+boardLayer.style.height = `${GRID_ROWS * (GRID_SIZE + GRID_GAP) - GRID_GAP}px`;
+
+startButton.addEventListener("click", startGame);
+retryButton.addEventListener("click", startGame);
+homeButton.addEventListener("click", goHome)
+skipButton.addEventListener("click", skip)
+
+window.addEventListener("resize", resizeGame);
+
+document.addEventListener('dblclick', function(e) {
+    e.preventDefault();
+}, { passive: false });
+
+addEventListners();
+
 function expandFormula(formula) {
     return [...formula.matchAll(/([A-Z][a-z]?)(\d*)/g)]
-        .flatMap(([_, element, count]) =>
-            Array(Number(count) || 1).fill(element)
-        );
+    .flatMap(([_, element, count]) =>
+        Array(Number(count) || 1).fill(element)
+);
 }
 
-function addEvent() {
-    document.addEventListener("pointerdown", (e) => {
-        if (!isPlaying || isGameOver) return;
-        const target = document.elementFromPoint(e.clientX, e.clientY);
-        if (target?.classList.contains("pieces")) {
-            isPointer = true;
-            addPiece(target);
-        }
+MOLECULES.forEach((e) => {
+    e.elements = expandFormula(e.formula)
+});
+
+function resizeGame() {
+    const scaleX = window.innerWidth / GAME_WIDTH;
+    const scaleY = window.innerHeight / GAME_HEIGHT;
+    const scale = Math.min(scaleX, scaleY);
+    gameLayer.style.transform = `scale(${scale})`;
+    return scale;
+}
+
+const GAME_SCALE = resizeGame();
+
+function startGame() {
+    clearBoard();
+
+    isPlaying = true;
+    isGameOver = false;
+
+    startScreen.classList.add("hidden");
+    gameOverScreen.classList.add("hidden");
+
+    titleLayer.textContent = "";
+    score = 0;
+    scoreLayer.textContent = score;
+
+    createInitialPieces();
+}
+
+function gameOver() {
+    if (isGameOver) return;
+
+    isGameOver = true;
+    isPlaying = false;
+
+    finalScore.textContent = `SCORE: ${score}`;
+    gameOverScreen.classList.remove("hidden");
+}
+
+function goHome() {
+    isPlaying = false;
+    isGameOver = false;
+
+    startScreen.classList.remove("hidden");
+    gameOverScreen.classList.add("hidden");
+}
+
+function skip() {
+    addNewpiece();
+}
+
+function clearBoard() {
+    document.querySelectorAll(".pieces, .lines").forEach((e) => {
+        e.remove();
     })
+    grid = Array.from(
+        {length: GRID_ROWS},
+        () => Array(GRID_COLS).fill(null)
+    );
+    selectedPieces.length = 0;
+}
 
-    document.addEventListener("pointerup", (e) => {
-        isPointer = false;
-        const molecule = isMolecule(selectPieces);
-        if (molecule === null) {
-            selectPieces.forEach((piece) => {
-                piece.style.filter = "brightness(1)";
-                piece.style.border = 
-                `3px solid ${ELEMENTS[piece.element].color}`;
-            });
-
-            selectPieces.length = 0;
-            document.querySelectorAll(".lines").forEach((e) => {
-                e.remove();
-            })
-
-            return;
-        }
-        
-        showMolecule(molecule.name); 
-        score += selectPieces.length ** 2;
-        scoreLayer.textContent = score;
-
-        selectPieces.forEach((piece) => {
-            grid[piece.gridY][piece.gridX] = null;
-            piece.remove();
-        })
-
-        selectPieces.length = 0;
-        document.querySelectorAll(".lines").forEach((e) => {
-            e.remove();
-        })
-    
-        newpiece();
+function updataGrid() {
+    const newGrid = Array.from(
+        {length: GRID_ROWS},
+        () => Array(GRID_COLS).fill(null)
+    );
+    document.querySelectorAll(".pieces").forEach((e) => {
+        newGrid[e.gridY][e.gridX] = e;
     })
+    return newGrid;
+}
 
-    document.addEventListener("pointermove", (e) => {
-        if (!isPlaying || isGameOver || !isPointer) return;
+function createInitialPieces() {
+    for (let col = 0; col < GRID_COLS; col++) {
+        for (let row = 0; row < 3; row++) {
+            createPiece(col, (GRID_ROWS - 1) - row, col * 40 + row * 180);
+        }
+    }
+}
 
-        const target = document.elementFromPoint(e.clientX, e.clientY);
-        if (!target?.classList.contains("pieces")) return;
-        if (selectPieces.includes(target)) {
-            const index = selectPieces.findIndex(i => i === target);
-            if (index === selectPieces.length - 2) {
-                const last = selectPieces[selectPieces.length - 1]
-                last.style.filter = "brightness(1)";
-                last.style.border = 
-                    `3px solid ${ELEMENTS[last.element].color}`;
-                selectPieces.splice(selectPieces.length - 1, 1)
-                document.querySelectorAll(".lines").forEach(element => {
-                    element.remove();
-                })
-                drawLine();
+function collapsePieces() {
+    for (let col = 0; col< GRID_COLS; col++) {
+        let row = GRID_ROWS - 1;
+        for (let y = GRID_ROWS - 1; y >= 0; y--) {
+            const piece = grid[y][col];
+            if (piece === null) continue;
+            if (row <= 0) {
+                gameOver();
             }
-            return;
+            piece.gridY = row;
+            row--;
         }
-        const gapX = Math.abs(target.gridX - selectPieces[selectPieces.length - 1].gridX);
-        const gapY = Math.abs(target.gridY - selectPieces[selectPieces.length - 1].gridY);
-        if (gapX + gapY !== 1) return;
-        document.querySelectorAll(".lines").forEach(element => {
-            element.remove();
-        })
-        addPiece(target);
-        drawLine();
-    })
+    }
+}
+
+function addNewpiece() {
+    for (let col = 0; col < GRID_COLS; col++) {
+        if (Math.random() < 0.3) continue;
+        createPiece(col, 0, 0);
+    }
+    collapsePieces();
+    grid = updataGrid();
+    document.querySelectorAll(".pieces").forEach((piece) => {
+        animatePiece(piece);
+    });
 }
 
 function createPiece(gridX, gridY, delay) {
@@ -303,71 +332,7 @@ function animatePiece(piece) {
     requestAnimationFrame(animate);
 }
 
-function animationButton(time) {
-    const scale = 1 + Math.sin(time * 0.005) * 0.05;
-    startButton.style.scale = scale;
-    retryButton.style.scale = scale;
-   requestAnimationFrame(animationButton);
-}
-
-function createInitialPieces() {
-    for (let col = 0; col < GRID_COLS; col++) {
-        for (let row = 0; row < 3; row++) {
-            createPiece(col, (GRID_ROWS - 1) - row, col * 40 + row * 180);
-        }
-    }
-}
-
-function clearBoard() {
-    document.querySelectorAll(".pieces, .lines").forEach((e) => {
-        e.remove();
-    })
-    grid = Array.from(
-        {length: GRID_ROWS},
-        () => Array(GRID_COLS).fill(null)
-    );
-    selectPieces.length = 0;
-}
-
-function startGame() {
-    clearBoard();
-
-    isPlaying = true;
-    isGameOver = false;
-
-    startScreen.classList.add("hidden");
-    gameOverScreen.classList.add("hidden");
-
-    titleLayer.textContent = "";
-    score = 0;
-    scoreLayer.textContent = score;
-
-    createInitialPieces();
-}
-
-function gameOver() {
-    if (isGameOver) return;
-
-    isGameOver = true;
-    isPlaying = false;
-
-    finalScore.textContent = `SCORE: ${score}`;
-    gameOverScreen.classList.remove("hidden");
-}
-
-function home() {
-    isPlaying = false;
-    isGameOver = false;
-
-    startScreen.classList.remove("hidden");
-    gameOverScreen.classList.add("hidden");
-}
-
-function skip() {
-    newpiece();
-}
-
-function addPiece(piece) {
+function selectPiece(piece) {
     const element = ELEMENTS[piece.element];
 
     piece.style.filter = "brightness(1.6)";
@@ -377,56 +342,47 @@ function addPiece(piece) {
     piece.scale = 1.2;
     animatePiece(piece);
 
-    selectPieces.push(piece);
+    selectedPieces.push(piece);
 }
 
-function showMolecule(name) {
-    clearTimeout(titleTimer);
-
-    titleLayer.textContent = name;
-    titleLayer.style.opacity = "1";
-
-    // titleTimer = setTimeout(() => {
-    //     titleLayer.style.opacity = "0";
-    // }, 2000);
-}
-
-function newpiece() {
-    for (let col = 0; col < GRID_COLS; col++) {
-        if (Math.random() < 0.3) continue;
-        createPiece(col, 0, 0);
-    }
-
-    for (let col = 0; col< GRID_COLS; col++) {
-        let row = GRID_ROWS - 1;
-        for (let y = GRID_ROWS - 1; y >= 0; y--) {
-            const piece = grid[y][col];
-            if (piece === null) continue;
-            if (row <= 0) {
-                gameOver();
-            }
-            piece.gridY = row;
-            row--;
-        }
-    }
-
-    const newGrid = Array.from(
-        {length: GRID_ROWS},
-        () => Array(GRID_COLS).fill(null)
-    );
-    document.querySelectorAll(".pieces").forEach((e) => {
-        newGrid[e.gridY][e.gridX] = e;
-    })
-    grid = newGrid;
-
-    document.querySelectorAll(".pieces").forEach((piece) => {
-        animatePiece(piece);
+function clearSelection() {
+    selectedPieces.forEach((piece) => {
+        piece.style.filter = 
+            "brightness(1)";
+        piece.style.border = 
+            `3px solid ${ELEMENTS[piece.element].color}`;
     });
+    selectedPieces.length = 0;
+}
+
+function removeSelectesPieces() {
+    selectedPieces.forEach((piece) => {
+        grid[piece.gridY][piece.gridX] = null;
+        piece.remove();
+    })
+    selectedPieces.length = 0;
+}
+
+function findMolecule(pieces) {
+    const selectedElements = pieces
+        .map((piece) => ELEMENTS[piece.element].text)
+        .sort();
+
+    return MOLECULES.find((molecule) => {
+        const moleculeElements = [...molecule.elements].sort(); 
+        return (
+            selectedElements.length === moleculeElements.length &&
+            selectedElements.every(
+                (element, index) =>
+                    element === moleculeElements[index]
+            )
+        );
+    }) ?? null;
 }
 
 function drawLine() {
-    for (let i = 1; i < selectPieces.length; i++) {
-        createLine(selectPieces[i].gridX, selectPieces[i].gridY, selectPieces[i - 1].gridX, selectPieces[i - 1].gridY)
+    for (let i = 1; i < selectedPieces.length; i++) {
+        createLine(selectedPieces[i].gridX, selectedPieces[i].gridY, selectedPieces[i - 1].gridX, selectedPieces[i - 1].gridY)
     }
 }
 
@@ -451,25 +407,82 @@ function createLine(x, y, x2, y2) {
         `${y * (GRID_SIZE + GRID_GAP) + GRID_SIZE / 2 + dy * (GRID_SIZE + GRID_GAP) / 2}px`;
 }
 
-function isMolecule(pieces) {
-    const selectedElements = pieces
-        .map((piece) => ELEMENTS[piece.element].text)
-        .sort();
-
-    return MOLECULES.find((molecule) => {
-        const moleculeElements = [...molecule.elements].sort(); 
-        return (
-            selectedElements.length === moleculeElements.length &&
-            selectedElements.every(
-                (element, index) =>
-                    element === moleculeElements[index]
-            )
-        );
-    }) ?? null;
+function clearLines() {
+    document.querySelectorAll(".lines").forEach((e) => {
+        e.remove();
+    })
 }
 
-MOLECULES.forEach((e) => {
-    e.elements = expandFormula(e.formula)
-});
-addEvent();
-requestAnimationFrame(animationButton);
+function addEventListners() {
+    document.addEventListener("pointerdown", (e) => {
+        if (!isPlaying || isGameOver) return;
+        const target = document.elementFromPoint(e.clientX, e.clientY);
+        if (target?.classList.contains("pieces")) {
+            isPointer = true;
+            selectPiece(target);
+        }
+    })
+
+    document.addEventListener("pointerup", (e) => {
+        isPointer = false;
+        const molecule = findMolecule(selectedPieces);
+        if (molecule === null) {
+            clearSelection();
+            clearLines();
+            return;
+        }
+        showMolecule(molecule.name); 
+        score += selectedPieces.length ** 2;
+        scoreLayer.textContent = score;
+        removeSelectesPieces();
+        clearLines();
+        addNewpiece();
+    })
+
+    document.addEventListener("pointermove", (e) => {
+        if (!isPlaying || isGameOver || !isPointer) return;
+        const target = document.elementFromPoint(e.clientX, e.clientY);
+        if (!target?.classList.contains("pieces")) return;
+        if (selectedPieces.includes(target)) {
+            const index = selectedPieces.findIndex(i => i === target);
+            if (index === selectedPieces.length - 2) {
+                const last = selectedPieces[selectedPieces.length - 1]
+                last.style.filter = "brightness(1)";
+                last.style.border = 
+                    `3px solid ${ELEMENTS[last.element].color}`;
+                selectedPieces.splice(selectedPieces.length - 1, 1)
+                clearLines();
+                drawLine();
+            }
+            return;
+        }
+        const gapX = Math.abs(target.gridX - selectedPieces[selectedPieces.length - 1].gridX);
+        const gapY = Math.abs(target.gridY - selectedPieces[selectedPieces.length - 1].gridY);
+        if (gapX + gapY !== 1) return;
+        document.querySelectorAll(".lines").forEach(element => {
+            element.remove();
+        })
+        selectPiece(target);
+        drawLine();
+    })
+}
+
+function showMolecule(name) {
+    clearTimeout(titleTimer);
+
+    titleLayer.textContent = name;
+    titleLayer.style.opacity = "1";
+
+    // titleTimer = setTimeout(() => {
+    //     titleLayer.style.opacity = "0";
+    // }, 2000);
+}
+
+function animateButtons(time) {
+    const scale = 1 + Math.sin(time * 0.005) * 0.05;
+    startButton.style.scale = scale;
+    retryButton.style.scale = scale;
+   requestAnimationFrame(animateButtons);
+}
+
+requestAnimationFrame(animateButtons);
